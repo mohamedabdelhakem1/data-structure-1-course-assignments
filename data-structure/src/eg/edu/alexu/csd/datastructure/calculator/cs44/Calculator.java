3f37d7e039390726209e53c3781a6cd8b75ac87a
SHIKO <SHIKO@SHIKO-PC>
2018-02-14 08:29:04 +0200

    26

--- data-structure/src/eg/edu/alexu/csd/datastructure/calculator/cs44/Calculator.java
+++ data-structure/src/eg/edu/alexu/csd/datastructure/calculator/cs44/Calculator.java
@@ -10,12 +10,12 @@ import eg.edu.alexu.csd.datastructure.calculator.ICalculator;
 public class Calculator implements ICalculator {
 
 	@Override
-	public int add(final int x, final int y) {
+	public int add(int x, int y) {
 		return 	(x + y);
 	}
 
 	@Override
-	public float divide(final int x, final int y) throws RuntimeException  {
+	public float divide(int x, int y) throws RuntimeException  {
 		if (y == 0) {
 			throw new RuntimeException("'Division by zero!'!");
 			}
