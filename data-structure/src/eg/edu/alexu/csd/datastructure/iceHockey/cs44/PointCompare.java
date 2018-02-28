7de401bac60602bd5ed0512e33a41e433945529f
SHIKO <SHIKO@SHIKO-PC>
2018-02-28 20:20:04 +0200

    9

--- data-structure/src/eg/edu/alexu/csd/datastructure/iceHockey/cs44/PointCompare.java
+++ data-structure/src/eg/edu/alexu/csd/datastructure/iceHockey/cs44/PointCompare.java
@@ -1,6 +1,8 @@
 package eg.edu.alexu.csd.datastructure.iceHockey.cs44;
+
 import java.awt.Point;
 import java.util.Comparator;
+
 /**
  *
  * @author SHIKO
@@ -9,19 +11,15 @@ import java.util.Comparator;
 public class PointCompare implements Comparator<Point> {
 
 	@Override
-	public int compare(final Point a,final Point b) {
+	public int compare(final Point a, final Point b) {
 		if (a.x < b.x) {
 			return -1;
-		}
-		else if (a.x > b.x) {
+		} else if (a.x > b.x) {
 			return 1;
-		}
-		else if (a.x == b.x)
-		{
+		} else if (a.x == b.x) {
 			if (a.y < b.y) {
 				return -1;
-			}
-			else if (a.y > b.y) {
+			} else if (a.y > b.y) {
 				return 1;
 			}
 		}
