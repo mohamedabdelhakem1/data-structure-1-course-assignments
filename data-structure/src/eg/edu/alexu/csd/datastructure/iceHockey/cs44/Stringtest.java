58761db7ba00f1945b17b38df06161e85d6d22b4
SHIKO <SHIKO@SHIKO-PC>
2018-03-02 18:12:57 +0200

    dsd

--- data-structure/src/eg/edu/alexu/csd/datastructure/iceHockey/cs44/Stringtest.java
+++ data-structure/src/eg/edu/alexu/csd/datastructure/iceHockey/cs44/Stringtest.java
@@ -15,7 +15,10 @@ public class Stringtest {
 
 	public void test1() {
 		Icehockey icehockey = new Icehockey();
-		String[] players = { "33JUBU33", "3U3O4433", "O33P44NB", "PO3NSDP3", "VNDSD333", "OINFD33X" };
+		String[] players = {"33JUBU33",
+			"3U3O4433", "O33P44NB",
+			"PO3NSDP3",
+			"VNDSD333", "OINFD33X" };
 		Point[] thepoints = icehockey.findPlayers(players, 3, 16);
 		Point[] point = new Point[] { new Point(4, 5), new Point(13, 9), new Point(14, 2) };
 		Assert.assertArrayEquals(point, thepoints);
@@ -24,8 +27,12 @@ public class Stringtest {
 	@Test
 	public void test2() {
 		Icehockey icehockey = new Icehockey();
-		String[] players = new String[] {"44444H44S4", "K444K4L444", "4LJ44T44XH", "444O4VIF44", "44C4D4U444",
-				"4V4Y4KB4M4", "G4W4HP4O4W", "4444ZDQ4S4", "4BR4Y4A444", "4G4V4T4444" };
+		String[] players = new String[] {"44444H44S4",
+				"K444K4L444", "4LJ44T44XH",
+				"444O4VIF44", "44C4D4U444",
+				"4V4Y4KB4M4", "G4W4HP4O4W",
+				"4444ZDQ4S4", "4BR4Y4A444",
+				"4G4V4T4444" };
 		Point[] thepoints = icehockey.findPlayers(players, 4, 16);
 		Point[] point = new Point[] { new Point(3, 8), new Point(4, 16), new Point(5, 4), new Point(16, 3),
 				new Point(16, 17), new Point(17, 9) };
@@ -35,9 +42,21 @@ public class Stringtest {
 	@Test
 	public void test3() {
 		Icehockey icehockey = new Icehockey();
-		String[] players = new String[] { "8D88888J8L8E888", "88NKMG8N8E8JI88", "888NS8EU88HN8EO", "LUQ888A8TH8OIH8",
-				"888QJ88R8SG88TY", "88ZQV88B88OUZ8O", "FQ88WF8Q8GG88B8", "8S888HGSB8FT8S8", "8MX88D88888T8K8",
-				"8S8A88MGVDG8XK8", "M88S8B8I8M88J8N", "8W88X88ZT8KA8I8", "88SQGB8I8J88W88", "U88H8NI8CZB88B8",
+		String[] players = new String[] {
+				"8D88888J8L8E888",
+				"88NKMG8N8E8JI88",
+				"888NS8EU88HN8EO",
+				"LUQ888A8TH8OIH8",
+				"888QJ88R8SG88TY",
+				"88ZQV88B88OUZ8O",
+				"FQ88WF8Q8GG88B8",
+				"8S888HGSB8FT8S8",
+				"8MX88D88888T8K8",
+				"8S8A88MGVDG8XK8",
+				"M88S8B8I8M88J8N",
+				"8W88X88ZT8KA8I8",
+				"88SQGB8I8J88W88",
+				"U88H8NI8CZB88B8",
 				"8PK8H8T8888TQR8" };
 		Point[] thepoints = icehockey.findPlayers(players, 8, 9);
 		Point[] point = new Point[] { new Point(1, 17), new Point(3, 3), new Point(3, 10), new Point(3, 25),
