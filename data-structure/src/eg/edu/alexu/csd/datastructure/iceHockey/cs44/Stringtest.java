9526837480551d8b6a3cb64aee81a07bc0443e52
SHIKO <SHIKO@SHIKO-PC>
2018-03-02 18:10:27 +0200

    122

--- data-structure/src/eg/edu/alexu/csd/datastructure/iceHockey/cs44/Stringtest.java
+++ data-structure/src/eg/edu/alexu/csd/datastructure/iceHockey/cs44/Stringtest.java
@@ -4,10 +4,15 @@ import java.awt.Point;
 
 import org.junit.Assert;
 import org.junit.Test;
-
+/**
+ *
+ * @author SHIKO
+ *
+ */
 public class Stringtest {
 
 	@Test
+
 	public void test1() {
 		Icehockey icehockey = new Icehockey();
 		String[] players = { "33JUBU33", "3U3O4433", "O33P44NB", "PO3NSDP3", "VNDSD333", "OINFD33X" };
@@ -19,11 +24,8 @@ public class Stringtest {
 	@Test
 	public void test2() {
 		Icehockey icehockey = new Icehockey();
-		String[] players = new String[] { "44444H44S4",
-				"K444K4L444",
-				"4LJ44T44XH", "444O4VIF44", "44C4D4U444",
-				"4V4Y4KB4M4", "G4W4HP4O4W", "4444ZDQ4S4",
-				"4BR4Y4A444", "4G4V4T4444" };
+		String[] players = new String[] {"44444H44S4", "K444K4L444", "4LJ44T44XH", "444O4VIF44", "44C4D4U444",
+				"4V4Y4KB4M4", "G4W4HP4O4W", "4444ZDQ4S4", "4BR4Y4A444", "4G4V4T4444" };
 		Point[] thepoints = icehockey.findPlayers(players, 4, 16);
 		Point[] point = new Point[] { new Point(3, 8), new Point(4, 16), new Point(5, 4), new Point(16, 3),
 				new Point(16, 17), new Point(17, 9) };
