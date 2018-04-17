c0b22afb87a63fbb93ff0d38dd5182403e1aa70a
SHIKO <SHIKO@SHIKO-PC>
2018-04-17 21:11:16 +0200

    xadasas

--- data-structure/src/eg/edu/alexu/csd/datastructure/linkedList/cs44_cs65/MyPolynomialSolverTest.java
+++ data-structure/src/eg/edu/alexu/csd/datastructure/linkedList/cs44_cs65/MyPolynomialSolverTest.java
@@ -8,26 +8,831 @@ import static org.junit.Assert.assertNull;
 import org.junit.Test;
 
 public class MyPolynomialSolverTest {
+	/**
+	*
+	*/
+	public final int n0 = 0;
+	/**
+	*
+	*/
+	public final int n1 = 1;
+	/**
+	*
+	*/
+	public final int n2 = 2;
+	/**
+	*
+	*/
+	public final int n3 = 3;
+	/**
+	*
+	*/
+	public final int n4 = 4;
+	/**
+	*
+	*/
+	public final int n5 = 5;
+	/**
+	*
+	*/
+	public final int n6 = 6;
+	/**
+	*
+	*/
+	public final int n7 = 7;
+	/**
+	*
+	*/
+	public final int n8 = 8;
+	/**
+	*
+	*/
+	public final int n9 = 9;
+	/**
+	*
+	*/
+	public final int n10 = 10;
+	/**
+	*
+	*/
+	public final int n11 = 11;
+	/**
+	*
+	*/
+	public final int n12 = 12;
+	/**
+	*
+	*/
+	public final int n13 = 13;
+	/**
+	*
+	*/
+	public final int n14 = 14;
+	/**
+	*
+	*/
+	public final int n15 = 15;
+	/**
+	*
+	*/
+	public final int n16 = 16;
+	/**
+	*
+	*/
+	public final int n17 = 17;
+	/**
+	*
+	*/
+	public final int n18 = 18;
+	/**
+	*
+	*/
+	public final int n19 = 19;
+	/**
+	*
+	*/
+	public final int n20 = 20;
+	/**
+	*
+	*/
+	public final int n21 = 21;
+	/**
+	*
+	*/
+	public final int n22 = 22;
+	/**
+	*
+	*/
+	public final int n23 = 23;
+	/**
+	*
+	*/
+	public final int n24 = 24;
+	/**
+	*
+	*/
+	public final int n25 = 25;
+	/**
+	*
+	*/
+	public final int n26 = 26;
+	/**
+	*
+	*/
+	public final int n27 = 27;
+	/**
+	*
+	*/
+	public final int n28 = 28;
+	/**
+	*
+	*/
+	public final int n29 = 29;
+	/**
+	*
+	*/
+	public final int n30 = 30;
+	/**
+	*
+	*/
+	public final int n31 = 31;
+	/**
+	*
+	*/
+	public final int n32 = 32;
+	/**
+	*
+	*/
+	public final int n33 = 33;
+	/**
+	*
+	*/
+	public final int n34 = 34;
+	/**
+	*
+	*/
+	public final int n35 = 35;
+	/**
+	*
+	*/
+	public final int n36 = 36;
+	/**
+	*
+	*/
+	public final int n37 = 37;
+	/**
+	*
+	*/
+	public final int n38 = 38;
+	/**
+	*
+	*/
+	public final int n39 = 39;
+	/**
+	*
+	*/
+	public final int n40 = 40;
+	/**
+	*
+	*/
+	public final int n41 = 41;
+	/**
+	*
+	*/
+	public final int n42 = 42;
+	/**
+	*
+	*/
+	public final int n43 = 43;
+	/**
+	*
+	*/
+	public final int n44 = 44;
+	/**
+	*
+	*/
+	public final int n45 = 45;
+	/**
+	*
+	*/
+	public final int n46 = 46;
+	/**
+	*
+	*/
+	public final int n47 = 47;
+	/**
+	*
+	*/
+	public final int n48 = 48;
+	/**
+	*
+	*/
+	public final int n49 = 49;
+	/**
+	*
+	*/
+	public final int n50 = 50;
+	/**
+	*
+	*/
+	public final int n51 = 51;
+	/**
+	*
+	*/
+	public final int n52 = 52;
+	/**
+	*
+	*/
+	public final int n53 = 53;
+	/**
+	*
+	*/
+	public final int n54 = 54;
+	/**
+	*
+	*/
+	public final int n55 = 55;
+	/**
+	*
+	*/
+	public final int n56 = 56;
+	/**
+	*
+	*/
+	public final int n57 = 57;
+	/**
+	*
+	*/
+	public final int n58 = 58;
+	/**
+	*
+	*/
+	public final int n59 = 59;
+	/**
+	*
+	*/
+	public final int n60 = 60;
+	/**
+	*
+	*/
+	public final int n61 = 61;
+	/**
+	*
+	*/
+	public final int n62 = 62;
+	/**
+	*
+	*/
+	public final int n63 = 63;
+	/**
+	*
+	*/
+	public final int n64 = 64;
+	/**
+	*
+	*/
+	public final int n65 = 65;
+	/**
+	*
+	*/
+	public final int n66 = 66;
+	/**
+	*
+	*/
+	public final int n67 = 67;
+	/**
+	*
+	*/
+	public final int n68 = 68;
+	/**
+	*
+	*/
+	public final int n69 = 69;
+	/**
+	*
+	*/
+	public final int n70 = 70;
+	/**
+	*
+	*/
+	public final int n71 = 71;
+	/**
+	*
+	*/
+	public final int n72 = 72;
+	/**
+	*
+	*/
+	public final int n73 = 73;
+	/**
+	*
+	*/
+	public final int n74 = 74;
+	/**
+	*
+	*/
+	public final int n75 = 75;
+	/**
+	*
+	*/
+	public final int n76 = 76;
+	/**
+	*
+	*/
+	public final int n77 = 77;
+	/**
+	*
+	*/
+	public final int n78 = 78;
+	/**
+	*
+	*/
+	public final int n79 = 79;
+	/**
+	*
+	*/
+	public final int n80 = 80;
+	/**
+	*
+	*/
+	public final int n81 = 81;
+	/**
+	*
+	*/
+	public final int n82 = 82;
+	/**
+	*
+	*/
+	public final int n83 = 83;
+	/**
+	*
+	*/
+	public final int n84 = 84;
+	/**
+	*
+	*/
+	public final int n85 = 85;
+	/**
+	*
+	*/
+	public final int n86 = 86;
+	/**
+	*
+	*/
+	public final int n87 = 87;
+	/**
+	*
+	*/
+	public final int n88 = 88;
+	/**
+	*
+	*/
+	public final int n89 = 89;
+	/**
+	*
+	*/
+	public final int n90 = 90;
+	/**
+	*
+	*/
+	public final int n91 = 91;
+	/**
+	*
+	*/
+	public final int n92 = 92;
+	/**
+	*
+	*/
+	public final int n93 = 93;
+	/**
+	*
+	*/
+	public final int n94 = 94;
+	/**
+	*
+	*/
+	public final int n95 = 95;
+	/**
+	*
+	*/
+	public final int n96 = 96;
+	/**
+	*
+	*/
+	public final int n97 = 97;
+	/**
+	*
+	*/
+	public final int n98 = 98;
+	/**
+	*
+	*/
+	public final int n99 = 99;
+	/**
+	*
+	*/
+	public final int n100 = 100;
+	/**
+	*
+	*/
+	public final int n101 = 101;
+	/**
+	*
+	*/
+	public final int n102 = 102;
+	/**
+	*
+	*/
+	public final int n103 = 103;
+	/**
+	*
+	*/
+	public final int n104 = 104;
+	/**
+	*
+	*/
+	public final int n105 = 105;
+	/**
+	*
+	*/
+	public final int n106 = 106;
+	/**
+	*
+	*/
+	public final int n107 = 107;
+	/**
+	*
+	*/
+	public final int n108 = 108;
+	/**
+	*
+	*/
+	public final int n109 = 109;
+	/**
+	*
+	*/
+	public final int n110 = 110;
+	/**
+	*
+	*/
+	public final int n111 = 111;
+	/**
+	*
+	*/
+	public final int n112 = 112;
+	/**
+	*
+	*/
+	public final int n113 = 113;
+	/**
+	*
+	*/
+	public final int n114 = 114;
+	/**
+	*
+	*/
+	public final int n115 = 115;
+	/**
+	*
+	*/
+	public final int n116 = 116;
+	/**
+	*
+	*/
+	public final int n117 = 117;
+	/**
+	*
+	*/
+	public final int n118 = 118;
+	/**
+	*
+	*/
+	public final int n119 = 119;
+	/**
+	*
+	*/
+	public final int n120 = 120;
+	/**
+	*
+	*/
+	public final int n121 = 121;
+	/**
+	*
+	*/
+	public final int n122 = 122;
+	/**
+	*
+	*/
+	public final int n123 = 123;
+	/**
+	*
+	*/
+	public final int n124 = 124;
+	/**
+	*
+	*/
+	public final int n125 = 125;
+	/**
+	*
+	*/
+	public final int n126 = 126;
+	/**
+	*
+	*/
+	public final int n127 = 127;
+	/**
+	*
+	*/
+	public final int n128 = 128;
+	/**
+	*
+	*/
+	public final int n129 = 129;
+	/**
+	*
+	*/
+	public final int n130 = 130;
+	/**
+	*
+	*/
+	public final int n131 = 131;
+	/**
+	*
+	*/
+	public final int n132 = 132;
+	/**
+	*
+	*/
+	public final int n133 = 133;
+	/**
+	*
+	*/
+	public final int n134 = 134;
+	/**
+	*
+	*/
+	public final int n135 = 135;
+	/**
+	*
+	*/
+	public final int n136 = 136;
+	/**
+	*
+	*/
+	public final int n137 = 137;
+	/**
+	*
+	*/
+	public final int n138 = 138;
+	/**
+	*
+	*/
+	public final int n139 = 139;
+	/**
+	*
+	*/
+	public final int n140 = 140;
+	/**
+	*
+	*/
+	public final int n141 = 141;
+	/**
+	*
+	*/
+	public final int n142 = 142;
+	/**
+	*
+	*/
+	public final int n143 = 143;
+	/**
+	*
+	*/
+	public final int n144 = 144;
+	/**
+	*
+	*/
+	public final int n145 = 145;
+	/**
+	*
+	*/
+	public final int n146 = 146;
+	/**
+	*
+	*/
+	public final int n147 = 147;
+	/**
+	*
+	*/
+	public final int n148 = 148;
+	/**
+	*
+	*/
+	public final int n149 = 149;
+	/**
+	*
+	*/
+	public final int n150 = 150;
+	/**
+	*
+	*/
+	public final int n151 = 151;
+	/**
+	*
+	*/
+	public final int n152 = 152;
+	/**
+	*
+	*/
+	public final int n153 = 153;
+	/**
+	*
+	*/
+	public final int n154 = 154;
+	/**
+	*
+	*/
+	public final int n155 = 155;
+	/**
+	*
+	*/
+	public final int n156 = 156;
+	/**
+	*
+	*/
+	public final int n157 = 157;
+	/**
+	*
+	*/
+	public final int n158 = 158;
+	/**
+	*
+	*/
+	public final int n159 = 159;
+	/**
+	*
+	*/
+	public final int n160 = 160;
+	/**
+	*
+	*/
+	public final int n161 = 161;
+	/**
+	*
+	*/
+	public final int n162 = 162;
+	/**
+	*
+	*/
+	public final int n163 = 163;
+	/**
+	*
+	*/
+	public final int n164 = 164;
+	/**
+	*
+	*/
+	public final int n165 = 165;
+	/**
+	*
+	*/
+	public final int n166 = 166;
+	/**
+	*
+	*/
+	public final int n167 = 167;
+	/**
+	*
+	*/
+	public final int n168 = 168;
+	/**
+	*
+	*/
+	public final int n169 = 169;
+	/**
+	*
+	*/
+	public final int n170 = 170;
+	/**
+	*
+	*/
+	public final int n171 = 171;
+	/**
+	*
+	*/
+	public final int n172 = 172;
+	/**
+	*
+	*/
+	public final int n173 = 173;
+	/**
+	*
+	*/
+	public final int n174 = 174;
+	/**
+	*
+	*/
+	public final int n175 = 175;
+	/**
+	*
+	*/
+	public final int n176 = 176;
+	/**
+	*
+	*/
+	public final int n177 = 177;
+	/**
+	*
+	*/
+	public final int n178 = 178;
+	/**
+	*
+	*/
+	public final int n179 = 179;
+	/**
+	*
+	*/
+	public final int n180 = 180;
+	/**
+	*
+	*/
+	public final int n181 = 181;
+	/**
+	*
+	*/
+	public final int n182 = 182;
+	/**
+	*
+	*/
+	public final int n183 = 183;
+	/**
+	*
+	*/
+	public final int n184 = 184;
+	/**
+	*
+	*/
+	public final int n185 = 185;
+	/**
+	*
+	*/
+	public final int n186 = 186;
+	/**
+	*
+	*/
+	public final int n187 = 187;
+	/**
+	*
+	*/
+	public final int n188 = 188;
+	/**
+	*
+	*/
+	public final int n189 = 189;
+	/**
+	*
+	*/
+	public final int n190 = 190;
+	/**
+	*
+	*/
+	public final int n191 = 191;
+	/**
+	*
+	*/
+	public final int n192 = 192;
+	/**
+	*
+	*/
+	public final int n193 = 193;
+	/**
+	*
+	*/
+	public final int n194 = 194;
+	/**
+	*
+	*/
+	public final int n195 = 195;
+	/**
+	*
+	*/
+	public final int n196 = 196;
+	/**
+	*
+	*/
+	public final int n197 = 197;
+	/**
+	*
+	*/
+	public final int n198 = 198;
+	/**
+	*
+	*/
+	public final int n199 = 199;
 
 	@Test
 	public void testSolveSubtracte() {
 		MyPolynomialSolver instance = new MyPolynomialSolver();
 		instance.setPolynomial('C',
-		new int[][] {{3, 7 }, {45, 5 }, {176, 3 }, {128, 1 } });
+		new int[][] {{n3, n7 }, {n45, n5 }, {n176, n3 }, {n128, n1 } });
 		instance.setPolynomial('B',
-		new int[][] {{-120, 5 }, {-1, 3 }, {27, 2 }, {1, 1 }, {-1, 0 } });
+		new int[][] {
+		{-n120, n5 },
+		{-n1, n3 },
+		{n27, n2 },
+		{n1, n1 },
+		{-n1, n0 } });
 
 		assertNull("Polynomial R is not set yet", instance.print('R'));
 		int[][] result1 = instance.subtract('C', 'B');
 		assertNotNull("Polynomial R must be set after evaluation",
 		instance.print('R'));
 		int[][] expected = new int[][] {
-		{3, 7 },
-		{165, 5 },
-		{177, 3 },
-		{-27, 2 },
-		{127, 1 },
-		{1, 0 } };
+		{n3, n7 },
+		{n165, n5 },
+		{n177, n3 },
+		{-n27, n2 },
+		{n127, n1 },
+		{n1, n0 } };
 		assertArrayEquals(expected, result1);
 	}
 
@@ -35,7 +840,7 @@ public class MyPolynomialSolverTest {
 	public void testprint1() {
 		MyPolynomialSolver instance = new MyPolynomialSolver();
 		instance.setPolynomial('C',
-		new int[][] {{3, 7 }, {45, 5 }, {176, 3 }, {128, 1 } });
+		new int[][] {{n3, n7 }, {n45, n5 }, {n176, n3 }, {n128, n1 } });
 		assertEquals(instance.print('C'), "3x^7+45x^5+176x^3+128x");
 
 	}
@@ -44,7 +849,12 @@ public class MyPolynomialSolverTest {
 	public void testprint2() {
 		MyPolynomialSolver instance = new MyPolynomialSolver();
 		instance.setPolynomial('C',
-		new int[][] {{-120, 5 }, {-1, 3 }, {27, 2 }, {1, 1 }, {-1, 0 } });
+		new int[][] {
+		{-n120, n5 },
+		{-n1, n3 },
+		{n27, n2 },
+		{n1, n1 },
+		{-n1, 0 } });
 		assertEquals(instance.print('C'), "-120x^5-x^3+27x^2+x-1");
 
 	}
@@ -60,10 +870,11 @@ public class MyPolynomialSolverTest {
 	@Test
 	public void testmultiply() {
 		MyPolynomialSolver instance = new MyPolynomialSolver();
-		instance.setPolynomial('C', new int[][] {{3, 7 }, {45, 5 } });
-		instance.setPolynomial('B', new int[][] {{-120, 5 }, {-1, 3 } });
+		instance.setPolynomial('C', new int[][] {{n3, n7 }, {n45, n5 } });
+		instance.setPolynomial('B', new int[][] {{-n120, n5 }, {-n1, n3 } });
 		int[][] result1 = instance.multiply('B', 'C');
-		int[][] expected = new int[][] {{-360, 12 }, {-5403, 10 }, {-45, 8 } };
+		int[][] expected =
+		new int[][] {{-n100 - n160, n12 }, {-n54 * n100 - n3, 10 }, {-45, 8 } };
 		assertEquals(instance.print('R'), "-360x^12-5403x^10-45x^8");
 		assertArrayEquals(result1, expected);
 	}
@@ -72,20 +883,25 @@ public class MyPolynomialSolverTest {
 	public void testSolveAdd() {
 		MyPolynomialSolver instance = new MyPolynomialSolver();
 		instance.setPolynomial('C',
-		new int[][] {{3, 7 }, {45, 5 }, {176, 3 }, {128, 1 } });
+		new int[][] {{n3, n7 }, {n45, n5 }, {n176, n3 }, {n128, n1 } });
 		instance.setPolynomial('B',
-		new int[][] {{-120, 5 }, {-1, 3 }, {27, 2 }, {1, 1 }, {-1, 0 } });
+		new int[][] {
+		{-n120, n5 },
+		{-n1, n3 },
+		{n27, n2 },
+		{n1, n1 },
+		{-n1, n0 } });
 		assertNull("Polynomial R is not set yet", instance.print('R'));
 		int[][] result1 = instance.add('B', 'C');
 		assertNotNull("Polynomial R must be set after evaluation",
 		instance.print('R'));
 		int[][] expected = new int[][] {
-		{3, 7 },
-		{-75, 5 },
-		{175, 3 },
-		{27, 2 },
-		{129, 1 },
-		{-1, 0 } };
+		{n3, n7 },
+		{-n75, n5 },
+		{n175, n3 },
+		{n27, n2 },
+		{n129, n1 },
+		{-n1, n0 } };
 		assertArrayEquals(expected, result1);
 	}
 
@@ -93,15 +909,19 @@ public class MyPolynomialSolverTest {
 	public void testSolveAdd2() {
 		MyPolynomialSolver instance = new MyPolynomialSolver();
 		instance.setPolynomial('C',
-		new int[][] {{0, 7 }, {45, 5 }, {176, 3 }, {128, 1 } });
+		new int[][] {{n0, n7 }, {n45, n5 }, {n176, n3 }, {n128, n1 } });
 		instance.setPolynomial('B',
-		new int[][] {{3, 7 }, {45, 5 }, {175, 3 }, {128, 1 } });
+		new int[][] {{n3, n7 }, {n45, n5 }, {n175, n3 }, {n128, n1 } });
 		assertNull("Polynomial R is not set yet", instance.print('R'));
 		int[][] result1 = instance.add('B', 'C');
 		assertNotNull("Polynomial R must be set after evaluation",
 		instance.print('R'));
 		int[][] expected =
-		new int[][] {{3, 7 }, {90, 5 }, {351, 3 }, {256, 1 } };
+		new int[][] {
+		{n3, n7 },
+		{n90, n5 },
+		{n100 + n100 + n100 + n51, n3 },
+		{n100 + n100 + n56, n1 } };
 		assertArrayEquals(expected, result1);
 	}
 
@@ -109,22 +929,22 @@ public class MyPolynomialSolverTest {
 	public void testSolveAdd3() {
 		MyPolynomialSolver instance = new MyPolynomialSolver();
 		instance.setPolynomial('C',
-		new int[][] {{3, 7 }, {45, 5 }, {176, 3 }, {128, 1 } });
+		new int[][] {{n3, n7 }, {n45, n5 }, {n176, n3 }, {n128, n1 } });
 		instance.setPolynomial('B',
-		new int[][] {{3, 7 }, {45, 5 }, {175, 3 }, {128, 1 } });
+		new int[][] {{n3, n7 }, {n45, n5 }, {n175, n3 }, {n128, n1 } });
 		assertNull("Polynomial R is not set yet", instance.print('R'));
 		int[][] result1 = instance.subtract('B', 'C');
 		assertNotNull("Polynomial R must be set after evaluation",
 		instance.print('R'));
-		int[][] expected = new int[][] {{-1, 3 } };
+		int[][] expected = new int[][] {{-n1, n3 } };
 		assertArrayEquals(expected, result1);
 	}
 
 	@Test
 	public void testSolveAdd4() {
 		MyPolynomialSolver instance = new MyPolynomialSolver();
-		instance.setPolynomial('B', new int[][] {{-3, 2 } });
-		instance.setPolynomial('C', new int[][] {{3, 2 } });
+		instance.setPolynomial('B', new int[][] {{-n3, n2 } });
+		instance.setPolynomial('C', new int[][] {{n3, n2 } });
 		assertNull("Polynomial R is not set yet", instance.print('R'));
 		int[][] result1 = instance.add('C', 'B');
 		assertNotNull("Polynomial R must be set after evaluation",
