e249c565cf476f67f69f61aa3ea3b9a21a9755ee
SHIKO <SHIKO@SHIKO-PC>
2018-04-17 13:27:10 +0200

    sdsds

---
+++ data-structure/src/eg/edu/alexu/csd/datastructure/stack/cs44/MainTest.java
@@ -0,0 +1,37 @@
+package eg.edu.alexu.csd.datastructure.stack.cs44;
+
+import java.util.Scanner;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		ExpressionEvaluator x = new ExpressionEvaluator();
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("enter the expression");
+		String str = s.nextLine();
+		System.out.println("enter 1 to evaluate the expression");
+		System.out.println("enter 2 to convert the expression");
+		int input = s.nextInt();
+		if (input == 1) {
+			try {
+				System.out.println(x.evaluate(str));
+			} catch (Exception e) {
+				System.out.println("invalid expression");
+			}
+		} else if (input == 2) {
+			try {
+				System.out.println(x.infixToPostfix(str));
+			} catch (Exception e) {
+				System.out.println("invalid expression");
+			}
+		}
+
+
+
+	}
+
+
+
+
+}
