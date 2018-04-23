a56412ccaa9f03326dd05f31cfaeffee7a0b845b
SHIKO <SHIKO@SHIKO-PC>
2018-04-23 19:59:25 +0200

    sdsd

--- data-structure/src/eg/edu/alexu/csd/datastructure/mailServer/cs65_cs42_cs44/MailServer.java
+++ data-structure/src/eg/edu/alexu/csd/datastructure/mailServer/cs65_cs42_cs44/MailServer.java
@@ -21,46 +21,44 @@ public class MailServer implements IApp {
 	 *
 	 */
 	@Override
-	public boolean signin(String email, String password) {
+	public boolean signin(final String email, final String password) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean signup(IContact contact) {
+	public boolean signup(final IContact contact) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-
 	@Override
-	public void setViewingOptions(IFolder folder, IFilter filter, ISort sort) {
+	public void setViewingOptions(final IFolder folder, final IFilter filter,
+	final ISort sort) {
 		// TODO Auto-generated method stub
 
 	}
 
-
 	@Override
-	public IMail[] listEmails(int page) {
+	public IMail[] listEmails(final int page) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void deleteEmails(ILinkedList mails) {
-		// TODO Auto-generated method stub
-
-	}
-
-
-	@Override
-	public void moveEmails(ILinkedList mails, IFolder des) {
+	public void deleteEmails(final ILinkedList mails) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public boolean compose(IMail email) {
+	public void moveEmails(final ILinkedList mails, final IFolder des) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean compose(final IMail email) {
 		// TODO Auto-generated method stub
 		return false;
 	}
